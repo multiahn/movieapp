bd81c14c140f5328b385c37a26d8c050f8fcbc67
multiahn <multiahn@gmail.com>
2020-12-16 11:42:21 +0900

    use state

--- src/App.js
+++ src/App.js
@@ -1,38 +1,27 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-function Potato({ name, id }) {
-  return <h1>I like {name} {id} </h1>;
-}
+class App extends React.Component {
+  state = {
+    count: 0
+  };
 
-Potato.propTypes = {
-  name: PropTypes.string.isRequired,
-  id: PropTypes.number.isRequired
-}
-
-const testArray = [
-  {
-    name: "kimchi",
-    id: 1
-  }, {
-    name: "ramen",
-    id: 2
-  }, {
-    name: "kimbap",
-    id: 3
+  add = () => {
+    this.setState(current => ({count: current.count +1}));
   }
-];
+  minus = () => {
+    this.setState(current => ({count: current.count -1}));
+  }
 
-function renderFood(dish) {
-  return <Potato key={dish.id} name={dish.name} id={dish.id} />;
-}
-
-function App() {
-  return (
-    <div>
-      {testArray.map(renderFood)}
-    </div>
-  );
+  render() {
+    return (
+      <div>
+        <h1>The number is {this.state.count}</h1>
+        <button onClick={this.add}>Add</button>
+        <button onClick={this.minus}>Minus</button>
+      </div>
+    );
+  }
 }
 
 export default App;
