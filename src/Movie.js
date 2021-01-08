feb9b80a7088bcad7ce5e37a991ca15f20892a05
multiahn <multiahn@gmail.com>
2021-01-08 11:01:52 +0900

    Modify Movie.js

--- src/Movie.js
+++ src/Movie.js
@@ -1,9 +1,18 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import "./Movie.css";
 
-function Movie({id, year, title, summary, poster}) {
+function Movie({id, year, title, summary, poster, genres}) {
     return (
-        <h4>{title}</h4>
+        <div className="movie">
+            <img src={poster} alt={title} title={title}/>
+            <div className="movie__data">
+                <h3 className="movie__title">{title}</h3>
+                <h5 className="movie__year">{year}</h5>
+                <ul className="genres">{genres.map((genre, index) => (<li key={index} className="genres__genre">{genre}</li>))}</ul>
+                <p className="movie__summary">{summary}</p>
+            </div>
+        </div>
     );
 }
 
@@ -12,5 +21,8 @@ Movie.propType = {
     year: PropTypes.number.isRequired,
     title: PropTypes.string.isRequired,
     summary: PropTypes.string.isRequired,
-    poster: PropTypes/string.isRequired
+    poster: PropTypes.string.isRequired,
+    genres: PropTypes.arrayOf(PropTypes.string).isRequired
 }
+
+export default Movie;
